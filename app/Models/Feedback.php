4335330d463cc2de86ffd91fc152188e031053b6
Timofey Accent <webokami@yandex.ru>
2023-11-15 12:47:36 +0300

    feedback

---
+++ app/Models/Feedback.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\Feedback
+ *
+ * @property int $id
+ * @property string $theme
+ * @property string $message
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Feedback newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Feedback newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Feedback query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Feedback whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Feedback whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Feedback whereMessage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Feedback whereTheme($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Feedback whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Feedback extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+}
